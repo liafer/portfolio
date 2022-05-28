4c37d9b23136e55b40347f7f06f31045a44969fd
Lia Workspace <LiaWorkspace@LIAs-MacBook-Air.local>
2022-05-28 17:07:22 -0400

    deploy portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { TiFlash } from 'react-icons/ti'
+import { BsSuitHeartFill } from 'react-icons/bs'
+import { BsCalculator } from 'react-icons/bs'
+import { FiLink } from 'react-icons/fi'
+import { TiWeatherCloudy } from 'react-icons/ti'
+import { IoMdCard } from 'react-icons/io'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-home">
+      <div className="main-home">
+        <h1>Hi, I'm Lia!</h1>
+        <h3>Front End developer</h3>
+        <p>Experience in web development. Passionate about creativity and technology.</p>
+        <p>Powered by high-impact web solutions.</p>
+        <TiFlash color='gold' size={40} />
+        <BsSuitHeartFill size={90} />
+        </div>
+
+        <div className='portfolio'>
+          <div className='projects-intro'>
+            <h2>Projects</h2>
+          </div>
+
+          <div className='projects'>
+            <div className='calculator'>
+              <span>Calculator App</span>
+              <BsCalculator size={70}/>
+              <a href="https://liafer.github.io/calculator-app" target="_blank" rel="external">View Project</a>
+            </div>
+
+            <div className='link'>
+              <span>URL shortener</span>
+              <FiLink size={70}/>
+              <a href="https://liafer.github.io/" target="_blank" rel="external">View Project</a>
+            </div>
+
+            <div className='weather'>
+              <span>Weather App</span>
+              <TiWeatherCloudy size={70}/>
+              <a href="https://liafer.github.io/weather-app" target="_blank" rel="external">View Project</a>
+            </div>
+
+            <div className='pricing'>
+              <span>Pricing Cards</span>
+              <IoMdCard size={70} />
+              <a href="https://liafer.github.io/pricing-component" target="_blank" rel="external">View Project</a>
+            </div>
+          </div>
+
+        </div>
+        
     </div>
   );
 }
